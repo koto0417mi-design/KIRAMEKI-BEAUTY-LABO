2676ddf6d733fa10cf235a240190208ce0eeedb5
miyu-togano <koto0417.mi@gmail.com>
2025-12-30 13:25:46 +0900

    全体修正２

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,20 +1,24 @@
-
+/* お客様の声 スワイパー */
 const swiper = new Swiper(".voice__slider", {
-    slidesPerView: 1.3,
-    spaceBetween:15,
-    centeredSlides:true,
-    initialSlide:1,
-      pagination: {
-    el: '.voice__pagination',
-    clickable: true,
-      },
+    slidesPerView: 1.26,
+    watchSlidesProgress: true,
+    roundLengths: true,
+    
+    spaceBetween: 15,
+    centeredSlides: true,
+    initialSlide: 1,
+    pagination: {
+        el: '.voice__pagination',
+        clickable: true,
+    },
 
-      breakpoints: {
-    768: {
-      slidesPerView: 3,
-      spaceBetween:15,
-      pagination: { enabled: false },
-      allowTouchMove:false,
-    }
-  },
+    breakpoints: {
+        768: {
+            slidesPerView: 3,
+            spaceBetween: 15,
+            pagination: { enabled: false },
+            allowTouchMove: false,
+        }
+    },
 });
+
